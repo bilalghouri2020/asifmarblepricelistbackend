e0c48644d0bca5003f39fcfce8669de50175948d
Raza Merchant <raza@idearebel.com>
2021-10-15 08:44:49 -0700

    commit 3.3

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@ var router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+
+
+
 
 /* GET users listing. */
 router.get('/', [
@@ -82,6 +88,64 @@ router.post('/', [
   } catch (err) {
     console.log(err);
   }
+})
+
+router.post('/login', [
+  check('email', 'Please enter a valid email...').isEmail(),
+  check('password', 'Password is required').exists()
+], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      console.log('400 error 2')
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        console.log('jwt')
+        if (err) throw err;
+        res.json({ token });
+      }
+    )
+
+
+
+
+  } catch (err) {
+    console.log(err, 'error found ')
+  }
+
+  console.log(req.body);
+  // console.log('hello world');
+  // res.json({ message: 'ok' })
 })
 
 module.exports = router;
